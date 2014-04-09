a20a658a0080718801d7040b29008d09bd044708
ezhulenev <Rlt4441444>
2014-04-09 14:00:57 -0400

    finagle protocol + examples

--- example/src/main/scala/com/fmdb/MonolithicExample.scala
+++ example/src/main/scala/com/fmdb/MonolithicExample.scala
@@ -6,21 +6,16 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 
 object MonolithicExample extends App {
-
   // Lets create services
-
   object Services extends PeopleServiceImpl with MoviesServiceImpl
 
   // Lets' get all movies for Leonardo DiCaprio
-
   val leo = Await.result(Services.people(), 1.second).find(_.firstName == "Leonardo").get
   val leoMovies = Await.result(Services.movies(leo), 1.second)
-
 
   println(s"Movies with $leo:")
   leoMovies.foreach(println)
 
   // Shutdown
-
   System.exit(0)
 }
