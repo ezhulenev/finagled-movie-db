2ff08baec12a7c1d65cc6431dacb5ab137a67982
ezhulenev <Rlt4441444>
2014-04-09 17:08:11 -0400

    update README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-finagled-movie-db
+Fancy Movie Database with Finagle
 =================
+
+Source code for blog posts:
+
+- [Seamless migration from monolithic application to Finagle services (Part 1/2)](http://eugenezhulenev.com/blog/2014/04/09/seamless-migration-from-monolithic-application-to-finagle-services-1)
+- [Seamless migration from monolithic application to Finagle services (Part 2/2)](http://eugenezhulenev.com/blog/2014/04/09/seamless-migration-from-monolithic-application-to-finagle-services-2)
--- core/src/main/scala/com/fmdb/distributed/FmdbServerConfig.scala
+++ core/src/main/scala/com/fmdb/distributed/FmdbServerConfig.scala
@@ -2,9 +2,27 @@ package com.fmdb.distributed
 
 import java.net.SocketAddress
 import com.twitter.finagle.Name
+import com.twitter.finagle.service.{TimeoutFilter, RetryPolicy, RetryingFilter}
+import com.twitter.finagle.util.DefaultTimer
+import com.twitter.util.Duration
 
 trait FmdbServerConfig {
   def serverAddress: SocketAddress
 
-  protected lazy val client = Fmdb.Client.newService(Name.bound(serverAddress), "fmdbClient")
+  import ServiceProtocol._
+
+  private[this] val retry = new RetryingFilter[FmdbReq, FmdbRep](
+    retryPolicy = RetryPolicy.tries(3),
+    timer = DefaultTimer.twitter
+  )
+
+  private[this] val timeout = new TimeoutFilter[FmdbReq, FmdbRep](
+    timeout = Duration.fromSeconds(10),
+    timer = DefaultTimer.twitter
+  )
+
+  protected lazy val client =
+    retry      andThen
+    timeout    andThen
+    Fmdb.Client.newService(Name.bound(serverAddress), "fmdbClient")
 }
